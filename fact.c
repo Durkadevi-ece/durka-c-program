c684666fd5981582c1291a40e591bb9c6832a9fc
Durkadevi-ece <durkadeviiyappan@gmail.com>
2025-06-28 22:38:54 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdio.h>
+void main(){
+int i,num,fact=1;
+printf("enter number");
+scanf("%d",&num);
+for(i=1;i<=num;i++){
+fact=fact*i;
+}
+printf("fact=%d",fact);
+
+
+
+
+}
